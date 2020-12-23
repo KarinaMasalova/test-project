baee6f804227b3ec7d92c0f0e4182c587d9fdaff
KarinaMasalova <Karyna.Masalava@LeverX.local>
2020-12-23 18:31:09 +0300

    feat: add dark mode

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,8 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
+import { Paper, Switch } from '@material-ui/core';
+import { ThemeProvider, createMuiTheme } from '@material-ui/core/styles';
+
 import './App.scss';
 
 export default function App() {
+    const [darkMode, setDarkMode] = useState(false);
+
+    const darkTheme = createMuiTheme({
+        palette: {
+            type: "dark"
+        }
+    });
+
+    const lightTheme = createMuiTheme({
+        palette: {
+            type: "light"
+        }
+    });
+
     return (
-        <div></div>
+        <ThemeProvider theme={darkMode ? darkTheme : lightTheme}>
+            <Paper style={{ height: "100vh" }}>
+                <Switch
+                    checked={darkMode}
+                    onChange={() => setDarkMode(!darkMode)}
+                />
+            </Paper>
+        </ThemeProvider>
     );
 }
