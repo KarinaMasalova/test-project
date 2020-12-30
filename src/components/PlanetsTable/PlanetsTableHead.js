b3e3c2dc7ccbee59e41e40c62af0dfadad8f6af5
KarinaMasalova <Karyna.Masalava@LeverX.local>
2020-12-30 10:14:04 +0300

    refactor: divide code into some folders

---
+++ src/components/PlanetsTable/PlanetsTableHead.js
@@ -0,0 +1,69 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import TableCell from '@material-ui/core/TableCell';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import TableSortLabel from '@material-ui/core/TableSortLabel';
+import Checkbox from '@material-ui/core/Checkbox';
+
+export default function EnhancedTableHead(props) {
+    const headCells = [
+        { id: 'name', numeric: false, disablePadding: false, label: 'Name' },
+        { id: 'diameter', numeric: true, disablePadding: false, label: 'Diameter' },
+        { id: 'climate', numeric: false, disablePadding: false, label: 'Climate' },
+        { id: 'terrain', numeric: false, disablePadding: false, label: 'Terrain' },
+        { id: 'population', numeric: true, disablePadding: false, label: 'Population' },
+        { id: 'created', numeric: false, disablePadding: false, label: 'Created' },
+        { id: 'edited', numeric: false, disablePadding: false, label: 'Edited' },
+    ];
+
+    const { classes, onSelectAllClick, order, orderBy, numSelected, rowCount, onRequestSort } = props;
+    const createSortHandler = (property) => (event) => {
+      onRequestSort(event, property);
+    };
+  
+    return (
+      <TableHead>
+        <TableRow>
+          <TableCell padding="checkbox">
+            <Checkbox
+              indeterminate={numSelected > 0 && numSelected < rowCount}
+              checked={rowCount > 0 && numSelected === rowCount}
+              onChange={onSelectAllClick}
+              inputProps={{ 'aria-label': 'select all desserts' }}
+            />
+          </TableCell>
+          {headCells.map((headCell) => (
+            <TableCell
+              key={headCell.id}
+              padding={headCell.disablePadding ? 'none' : 'default'}
+              sortDirection={orderBy === headCell.id ? order : false}
+            >
+              <TableSortLabel
+                active={orderBy === headCell.id}
+                direction={orderBy === headCell.id ? order : 'asc'}
+                onClick={createSortHandler(headCell.id)}
+              >
+                {headCell.label}
+                {orderBy === headCell.id ? (
+                  <span className={classes.visuallyHidden}>
+                    {order === 'desc' ? 'sorted descending' : 'sorted ascending'}
+                  </span>
+                ) : null}
+              </TableSortLabel>
+            </TableCell>
+          ))}
+        </TableRow>
+      </TableHead>
+    );
+}
+  
+EnhancedTableHead.propTypes = {
+    classes: PropTypes.object.isRequired,
+    numSelected: PropTypes.number.isRequired,
+    onRequestSort: PropTypes.func.isRequired,
+    onSelectAllClick: PropTypes.func.isRequired,
+    order: PropTypes.oneOf(['asc', 'desc']).isRequired,
+    orderBy: PropTypes.string.isRequired,
+    rowCount: PropTypes.number.isRequired,
+};
