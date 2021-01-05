e25246621cd6774d7a0375e718206707c10af182
KarinaMasalova <Karyna.Masalava@LeverX.local>
2021-01-05 15:00:17 +0300

    feat: routing

---
+++ src/containers/Main/ChartsOverview/ChartsOverview.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+import '../Main.scss';
+
+export default function ChartsContent() {
+    return (
+      <main className="main">
+        <div>Charts</div>
+      </main>
+    );
+}
---
+++ src/containers/Main/LawyersOverview/LawyersOverview.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+import '../Main.scss';
+import LawyersTable from '../../../components/LawyersTable/LawyersTable';
+
+export default function LawyersTableContent() {
+    return (
+      <main className="main">
+        <LawyersTable />
+      </main>
+    );
+}
