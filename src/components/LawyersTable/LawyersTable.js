8f2e10662aac4c3981833129a1b9acd4b8863871
KarinaMasalova <Karyna.Masalava@LeverX.local>
2021-01-05 22:07:11 +0300

    refactor: replace concat with spread operator

--- src/components/LawyersTable/LawyersTable.js
+++ src/components/LawyersTable/LawyersTable.js
@@ -61,16 +61,17 @@ export default function LawyersTable() {
     let newSelected = [];
 
     if (selectedIndex === -1) {
-      newSelected = newSelected.concat(selected, name);
+      newSelected = [...newSelected, ...selected, name];
     } else if (selectedIndex === 0) {
-      newSelected = newSelected.concat(selected.slice(1));
+      newSelected = [...newSelected, ...selected.slice(1)];
     } else if (selectedIndex === selected.length - 1) {
-      newSelected = newSelected.concat(selected.slice(0, -1));
+      newSelected = [...newSelected, ...selected.slice(0, -1)];
     } else if (selectedIndex > 0) {
-      newSelected = newSelected.concat(
-        selected.slice(0, selectedIndex),
-        selected.slice(selectedIndex + 1),
-      );
+      newSelected = [
+        ...newSelected,
+        ...selected.slice(0, selectedIndex),
+        ...selected.slice(selectedIndex + 1),
+      ];
     }
 
     setSelected(newSelected);
