14e4a232007af723730a747430a8c2560d777620
maksim.zalutski@leverx.com <Karyna.Masalava@LeverX.local>
2021-01-11 16:37:55 +0300

    fix: paper height

--- src/containers/App.js
+++ src/containers/App.js
@@ -3,19 +3,23 @@ import { Route } from 'react-router-dom';
 import SwipeableRoutes from "react-swipeable-routes";
 
 import { Paper } from '@material-ui/core';
-import { ThemeProvider } from '@material-ui/core/styles';
+import { ThemeProvider, makeStyles } from '@material-ui/core/styles';
 
 import getTheme from '../utils/theme';
 import Context from '../utils/context';
 
 import './App.scss';
+import getAppStyles from '../styles';
 import Header from './Header/Header';
 import PeopleOverview from './Main/PeopleOverview/PeopleOverview';
 import ChartsOverview from './Main/ChartsOverview/ChartsOverview';
 import ErrorSnackbar from '../components/ErrorSnackbar/ErrorSnackbar';
 
+const useStyles = makeStyles(getAppStyles);
+
 export default function App() {
   const theme = getTheme();
+  const classes= useStyles();
   const isDarkMode = (theme) => theme.palette.type === 'dark';
   const [darkMode, setDarkMode] = useState(isDarkMode(theme));
 
@@ -38,7 +42,7 @@ export default function App() {
   return (
     <ThemeProvider theme={{...theme, palette: {...theme.palette, type:  darkMode ? 'dark' : 'light' } }}>
       <Context.Provider value={{darkMode, switchMode}}>
-        <Paper square>
+        <Paper square classes={classes}>
           <Header />
           <SwipeableRoutes>
             <Route exact path="/charts" component={ChartsOverview} />
---
+++ src/styles.js
@@ -0,0 +1,10 @@
+
+const getAppStyles = (theme) => ({
+    root: {
+      paddingLeft: theme.spacing(2),
+      paddingRight: theme.spacing(1),
+      height: '100%'
+    },
+  });
+
+export default getAppStyles;
