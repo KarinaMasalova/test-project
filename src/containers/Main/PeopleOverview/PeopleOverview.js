1d14cda4314869b99d062f8a98f18990a2a27420
KarinaMasalova <Karyna.Masalava@LeverX.local>
2021-01-12 11:06:54 +0300

    refactor: filters

--- src/containers/Main/PeopleOverview/PeopleOverview.js
+++ src/containers/Main/PeopleOverview/PeopleOverview.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 
 import { makeStyles } from '@material-ui/core/styles';
@@ -16,68 +16,54 @@ import * as constants from '../../../constants/constants';
 
 const useStyles = makeStyles(getPeopleOverviewStyles);
 
-let name = '';
-let location = '';
-let age = null;
-let role = null;
-
 export default function PeopleOverview() {
   const classes = useStyles();
   const dispatch = useDispatch();
   const allPeopleData = useSelector(getAllPeople);
-  const [ageState, setAge] = useState({label: ''});
-  const [roleState, setRole] = useState({label: ''});
+  const [filters, setFilters] = useState({
+    name: '',
+    location: '',
+    age: '',
+    role: ''
+  });
 
-  const filter = () => {
+  useEffect(() => filterTable(), [filters]);
+
+  const filterTable = () => {
+    const ageMax = constants.ageOptions.filter((obj) => obj.label === filters.age)[0];
     const filteredData = allPeopleData.filter((obj) => {
-      return (obj.firstName.toLowerCase().includes(name.toLowerCase()) || obj.lastName.toLowerCase().includes(name.toLowerCase())) &&
-        (obj.city.toLowerCase().includes(location.toLowerCase()) || obj.country.toLowerCase().includes(location.toLowerCase())) &&
-        (age === null || obj.age < age) &&
-        (role === null || obj.role === role)
+      return (obj.firstName.toLowerCase().includes(filters.name.toLowerCase()) ||
+          obj.lastName.toLowerCase().includes(filters.name.toLowerCase())) &&
+        (obj.city.toLowerCase().includes(filters.location.toLowerCase()) ||
+          obj.country.toLowerCase().includes(filters.location.toLowerCase())) &&
+        (ageMax === undefined || obj.age < ageMax.maxValue) &&
+        (filters.role === '' || obj.role === filters.role)
     });
     dispatch(setFilteredPeople(filteredData));
   }
 
-  const handleSelectAgeChange = (e) => {
-    const ageObj = constants.ageOptions.filter((obj) => obj.label === e.target.value)[0];
-    age = ageObj.maxValue;
-    setAge(ageObj);
-    filter();
-  }
-
-  const handleSelectRoleChange = (e) => {
-    const roleObj = constants.roleOptions.filter((obj) => obj.label === e.target.value)[0];
-    role = roleObj.label;
-    setRole(roleObj);
-    filter();
-  }
-
-  const handleInputNameChange = (e) => {
-    name = e.target.value;
-    filter();
-  }
-
-  const handleInputLocationChange = (e) => {
-    location = e.target.value;
-    filter();
+  const handleFilterChange = (e, filter) => {
+    const obj = { ...filters };
+    obj[filter] = e.target.value;
+    setFilters(obj);
   }
 
   return (
     <main className="main">
-      <Input onChange={handleInputNameChange} label="Enter firstname / lastname..." />
-      <Input onChange={handleInputLocationChange} label="Enter country / city..." />
+      <Input onChange={(e) => handleFilterChange(e, 'name')} label="Enter firstname / lastname..." />
+      <Input onChange={(e) => handleFilterChange(e, 'location')} label="Enter country / city..." />
       <div className={classes.root}>
         <Select
           options={constants.ageOptions.map((obj) => obj.label)}
-          onChange={handleSelectAgeChange}
+          onChange={(e) => handleFilterChange(e, 'age')}
           label="Select age..."
-          value={ageState.label}
+          value={filters.age}
         />
         <Select
           options={constants.roleOptions.map((obj) => obj.label)}
-          onChange={handleSelectRoleChange}
+          onChange={(e) => handleFilterChange(e, 'role')}
           label="Select role..."
-          value={roleState.label}
+          value={filters.role}
         />
       </div>
       <Button variant="outlined" color="secondary" value="Reset filters" />
