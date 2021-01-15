0f651e89163a3a631052d05c58fc2001a77c8d92
KarinaMasalova <Karyna.Masalava@LeverX.local>
2021-01-15 16:10:59 +0300

    feat: make logo as a link

--- src/containers/Header/Header.js
+++ src/containers/Header/Header.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 import { makeStyles } from '@material-ui/core/styles';
 
@@ -12,9 +13,9 @@ export default function Header() {
   const classes = useStyles();
   return (
     <header className={classes.root}>
-      <div className={classes.logo}>
+      <Link to="/" className={classes.logo}>
         <span className={classes.logoImage} />
-      </div>
+      </Link>
       <HeaderTabPanel />
       <HeaderOptionsMenu />
     </header>
